601d3d031de742d0bbe5d16f93a2cccc038f4de7
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-09-21 16:36:10 +0200

    .

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.addEventListener("load", loadColors);
 // animate visibility
 const observer = new IntersectionObserver((entries) => [
   entries.forEach((entry) => {
@@ -11,4 +12,29 @@ const observer = new IntersectionObserver((entries) => [
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((element) => observer.observe(element));
+
+const sel = document.querySelector("select");
+sel.addEventListener("change", clrSwitch);
+const themeValue = localStorage.getItem("theme");
 // animate visibility
+function clrSwitch() {
+  if (sel.value === "dark") {
+    document.querySelector("body").dataset.theme = "dark";
+    localStorage.setItem("theme", sel.value);
+  } else if (sel.value === "light") {
+    document.querySelector("body").dataset.theme = "light";
+    localStorage.setItem("theme", sel.value);
+  } else if (sel.value === "dust") {
+    document.querySelector("body").dataset.theme = "dust";
+    localStorage.setItem("theme", sel.value);
+  }
+}
+function loadColors() {
+  if (themeValue) {
+    document.querySelector("body").dataset.theme = themeValue;
+    sel.value = themeValue;
+  } else {
+    document.querySelector("body").dataset.theme = "dark";
+  }
+  // console.log("***********");
+}
